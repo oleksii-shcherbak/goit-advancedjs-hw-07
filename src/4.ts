a0437cb08f85b98715e633afc1528463d40cf13b
Oleksii Shcherbak <oleksii_shcherbak@icloud.com>
2025-11-13 16:44:04 +0200

    Completed HW

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,52 @@
+class Key {
+  constructor(private signature: number = Math.random()) {}
+
+  getSignature(): number {
+    return this.signature;
+  }
+}
+
+class Person {
+  constructor(private key: Key) {}
+
+  getKey(): Key {
+    return this.key;
+  }
+}
+
+abstract class House {
+  protected door: boolean;
+  protected tenants: Person[];
+
+  constructor(protected key: Key) {
+    this.door = false;
+    this.tenants = [];
+  }
+
+  comeIn(person: Person): void {
+    if (this.door) {
+      this.tenants.push(person);
+      this.door = false;
+      console.log('The person is in the house. They closed the door behind them.');
+    } else {
+      console.log('The door is locked! This person is not able to come in.');
+    }
+  }
+
+  abstract openDoor(key: Key): void;
+}
+
+class MyHouse extends House {
+  openDoor(key: Key): void {
+    if (key.getSignature() === this.key.getSignature()) {
+      this.door = true;
+      console.log('The door is opened with the key.');
+    } else {
+      console.log('You have got the wrong key! The door is locked.');
+    }
+  }
+}
+
 const key = new Key();
 
 const house = new MyHouse(key);
@@ -6,6 +55,12 @@ const person = new Person(key);
 house.openDoor(person.getKey());
 
 house.comeIn(person);
+
+// Another person with wrong key
+const key1 = new Key();
+const person1 = new Person(key1);
+house.openDoor(person1.getKey());
+house.comeIn(person1);
 
 
 export {};
